59bc27f48fef175a30817e3f01f5c5578bdef19a
Wanderson-Valentim <wanderson200108@gmail.com>
2025-10-30 11:40:06 -0300

    feat(repository): implement GuildSettings repository
    Introduces the repository pattern to abstract database interactions for the 'GuildSettings' model.

---
+++ repositories/guildSettingsRepository.js
@@ -0,0 +1,22 @@
+const { GuildSettings } = require('../database.js');
+
+async function getOrCreate(guildId) {
+	return await GuildSettings.findOrCreate({
+		where: { guild_id: guildId },
+		defaults: { guild_id: guildId },
+	});
+}
+
+async function update(guildId, propertiesToUpdate) {
+	return await GuildSettings.update(
+		propertiesToUpdate,
+		{
+			where: { guild_id: guildId },
+		},
+	);
+}
+
+module.exports = {
+	getOrCreate,
+	update,
+};
