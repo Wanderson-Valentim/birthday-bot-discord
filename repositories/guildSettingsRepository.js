50759d8462fafff3df378e3daa87b163c26d8f77
Wanderson-Valentim <wanderson200108@gmail.com>
2025-11-07 09:31:47 -0300

    feat(repository): add destroy and getAllGuildIds methods

--- repositories/guildSettingsRepository.js
+++ repositories/guildSettingsRepository.js
@@ -24,6 +24,21 @@ class GuildSettingsRepository {
 			where: { guild_id: guildId },
 		});
 	}
+
+	async destroy(guildId) {
+		console.log(`[DB] Deleting all data for guild ${guildId}...`);
+		return await this.GuildSettings.destroy({
+			where: { guild_id: guildId },
+		});
+	}
+
+	async getAllGuildIds() {
+		const guilds = await this.GuildSettings.findAll({
+			attributes: ['guild_id'],
+			raw: true,
+		});
+		return guilds.map(g => g.guild_id);
+	}
 }
 
 module.exports = GuildSettingsRepository;
